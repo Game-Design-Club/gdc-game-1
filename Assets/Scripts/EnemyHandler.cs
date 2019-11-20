8e82ec870d771e76bacef049b08d2c219b66ea5a
Cameron <36940948+PawstaCat@users.noreply.github.com>
2019-11-19 20:49:03 -0800

    Add files via upload

---
+++ Assets/Scripts/EnemyHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour
+{
+	[SerializeField]
+	float speed = 11f;
+	[SerializeField]
+	GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if(player != null)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, new Vector2(player.transform.position.x, player.transform.position.y), speed * Time.deltaTime);
+		}
+    }
+}
