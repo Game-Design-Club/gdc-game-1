e04debe3cd1976a7f0f510fed8fdb46be6ed0875
Decrypted-X <56646209+Decrypted-X@users.noreply.github.com>
2019-12-02 20:08:39 -0800

    Add files via upload

---
+++ Assets/Scripts/ItemColliDetect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemColliDetect : MonoBehaviour
+{
+    private ItemHandler itemHandler;
+
+    void Start()
+    {
+        itemHandler = transform.parent.GetComponent<ItemHandler>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        itemHandler.setPickUp(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        itemHandler.setPickUp(false);
+    }
+}
