5b91b4251bd7cbc701b79f2351be64674b48aaab
ZillAnimu <nintendof4b0y@gmail.com>
2020-01-21 19:47:58 -0800

    Updated Files for Vision
    Added a Vision object to the Enemy Handler for action boolean
    Should print out words into console upon successful (Die) or unsuccessful (oh no) detection

---
+++ Assets/Scripts/Enemy Stuff/EnemyHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour
+{
+	[SerializeField]
+	float speed = 11f;
+	[SerializeField]
+	GameObject player;
+    [SerializeField]
+    EnemyVision FOV;
+    private void FixedUpdate()
+    {
+        if(FOV.getPlayerSighted())
+		{
+			transform.position = Vector2.MoveTowards(transform.position, new Vector2(this.player.transform.position.x, this.player.transform.position.y), speed * Time.deltaTime);
+            float plrDistance = this.player.transform.position.x - transform.position.x;
+            if (plrDistance < 0f)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            else if(plrDistance > 0f)
+            {
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+		}
+    }
+}
---
+++ Assets/Scripts/Enemy Stuff/EnemyVision.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** 
+ * Bugs:
+ * If player too close to the ground collision, enemies still detect
+ **/
+
+public class EnemyVision : MonoBehaviour
+{
+    [SerializeField]
+    private CircleCollider2D hitbox;
+    [SerializeField]
+    private GameObject player;
+    private BoxCollider2D playerBox;
+    private bool playerSighted;
+    private List<RaycastHit2D> results;
+    private ContactFilter2D filter;
+    private void Start()
+    {
+        playerSighted = false;
+        playerBox = (BoxCollider2D)player.GetComponent(typeof(BoxCollider2D));
+        results = new List<RaycastHit2D>();
+    }
+
+    public bool getPlayerSighted()
+    {
+        return playerSighted;
+    }
+
+    private bool groundBetween(List<RaycastHit2D> results)
+    {
+        foreach (RaycastHit2D result in results)
+        {
+            if (result.collider.gameObject.CompareTag("Player"))
+            {
+                return false;
+            }
+            if (result.collider.gameObject.CompareTag("Ground"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (hitbox.IsTouching(playerBox))
+        {
+            //detecting itself instead of next object
+            Physics2D.Raycast(hitbox.transform.position, player.transform.position, filter, results, hitbox.radius);
+            if (groundBetween(results))
+            {
+                playerSighted = false;
+                print("Oh no");
+            }
+            else {
+                playerSighted = true;
+                print("Die");
+            }
+        }
+    }
+}
