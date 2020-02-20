ca0f78d9b16ae99359e5fdc2c5a2006bfb6683f6
Orau-Areya <59894129+Orau-Areya@users.noreply.github.com>
2020-02-20 12:32:40 -0800

    Add files via upload

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -7,22 +7,25 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float horizontalSpeed = 10f;
     [SerializeField] float sprintMultiplyer = 1f;
-	[SerializeField] float jumpPower = 8f; 
+	[SerializeField] float jumpPower = 10f; 
 	[SerializeField] float gravityScale = 1f;
 	[SerializeField] float scaleDeducter = 0.1f;
 	[SerializeField] float bufferMax = 0.25f;
-	[SerializeField] float dodgeSpeed = 1f;
-	[SerializeField] float dodgeCooldown = 3f;
+	[SerializeField] float dodgeModifier = .3df; //keep this below 1f please
+	[SerializeField] readonly float dodgeTimeTop = 2f;
+	[SerializeField]  float dodgeTime = 0f;
 
 	[SerializeField] Rigidbody2D rb;
+    [SerializeField] Rigidbody2D player;
 
 	private float bufferTimer = 100f;
 
-	private bool isJumping;
+	public bool isJumping;
 	private bool isGrounded;
     private bool isSprinting;
+	[SerializeField] private bool canDodge = true;
 
-	private bool canDodge = true;
+	//private bool canDodge = true;
 
 	private void Start()
 	{
@@ -41,92 +44,83 @@ public class PlayerMovement : MonoBehaviour
 		Jump();
     }
 
-    private void ProcessOtherInputs() 
-    {
-        if (Input.GetKeyDown(KeyCode.LeftShift) && !isSprinting )
-        {
-            // Allows player to sprint if not already sprinting
-            sprintMultiplyer = (float)(sprintMultiplyer * 1.5);
-            // Changes the sprint multiplyer from 1 to 1.5, allowing the player to move 50% faster.
-            isSprinting = true;
-        }
-
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            sprintMultiplyer = 1f;
-            isSprinting = false;
-            //todo check that sprint still works, if not delete isSprinting = false;
-        }
-
-		if (canDodge && Input.GetKeyDown(KeyCode.P))
+	private void ProcessOtherInputs()
+	{
+		if (Input.GetKeyDown(KeyCode.LeftShift) && !isSprinting)
 		{
-
-			if (!isGrounded)
-			{
-				Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-
-				transform.position += movement * (dodgeSpeed / 1.5f) * Time.deltaTime * horizontalSpeed;
-			}
-			else
-			{ 
-			    Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-
-			    transform.position += movement * dodgeSpeed * Time.deltaTime * horizontalSpeed;
-		    }
-			Invoke("SecondDodge", .01f);
-
-			Invoke("SecondDodge", .01f);
-
-			canDodge = false;
-
-            Invoke("AllowDodge", dodgeCooldown);
+			// Allows player to sprint if not already sprinting
+			sprintMultiplyer = (float)(sprintMultiplyer * 1.5);
+			// Changes the sprint multiplyer from 1 to 1.5, allowing the player to move 50% faster.
+			isSprinting = true;
 		}
 
-		if (Input.GetKeyUp(KeyCode.P))
+		if (Input.GetKeyDown(KeyCode.P) && canDodge)
+		{
+			Dodge();
+		}
+
+		//if (Input.GetKeyUp(KeyCode.P) || !canDodge)
+		//{
+		//	sprintMultiplyer = 1f;
+		//	isDodging = false;
+		//	//todo check that sprint still works, if not delete isSprinting = false;
+		//}
+
+		if (Input.GetKeyUp(KeyCode.LeftShift))
 		{
 			sprintMultiplyer = 1f;
+			isSprinting = false;
+			//todo check that sprint still works, if not delete isSprinting = false;
 		}
 	}
 
-    private void MovePlayer()
-    {
-        // Records player horizontal movement factor, then moves the player
-        // Should work for controller?
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * sprintMultiplyer * Time.deltaTime * horizontalSpeed;
-        // The two line above actually move the player.
+	private void MovePlayer()
+	{
+		// Records player horizontal movement factor, then moves the player
+		// Should work for controller?
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+		transform.position += movement * sprintMultiplyer * Time.deltaTime * horizontalSpeed;
+		// The two line above actually move the player.
 
-        // Flips the character based on which way it moves.
-        if (Input.GetAxis("Horizontal") < 0f)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-        }
-        else if (Input.GetAxis("Horizontal") > 0f)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
-    }
+		// Flips the character based on which way it moves.
+		if (Input.GetAxis("Horizontal") < 0f)
+		{
+			gameObject.GetComponent<SpriteRenderer>().flipX = true;
+		}
+		else if (Input.GetAxis("Horizontal") > 0f)
+		{
+			gameObject.GetComponent<SpriteRenderer>().flipX = false;
+		}
+	}
 
-    private void AllowDodge()
+	private void Dodge()
+	{
+        float direction = Input.GetAxisRaw("Horizontal");
+        Vector3 movement = new Vector3(direction, 0f, 0f);
+
+        for (float i = 0; i <= 1f; i += Time.fixedDeltaTime)
+        {
+            transform.position += movement * dodgeModifier * Time.deltaTime * horizontalSpeed;
+        }
+
+        //dodgeCooldown(); Having trouble on the cooldown for Dodge, but the rest is good (2/17/2020)
+	}
+
+    private void dodgeCooldown()
     {
+        dodgeTime = 0f;
+
+		while (dodgeTime <= dodgeTimeTop)
+        {
+			dodgeTime += Time.deltaTime;
+
+            canDodge = false;
+        }
+
 		canDodge = true;
+
+		dodgeTime = 0f;
     }
-
-    private void SecondDodge()
-    {
-		if (!isGrounded)
-		{
-			Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-
-			transform.position += movement * (dodgeSpeed / 1.5f) * Time.deltaTime * horizontalSpeed;
-		}
-		else
-		{
-			Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-
-			transform.position += movement * dodgeSpeed * Time.deltaTime * horizontalSpeed;
-		}
-	}
 
 	private void Jump()
 	{
@@ -135,7 +129,7 @@ public class PlayerMovement : MonoBehaviour
 			isJumping = false;
 		}
 
-		if(((Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.W)) || bufferTimer <= bufferMax) && isGrounded)
+		if((Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.W) || bufferTimer <= bufferMax) && isGrounded)
 		{
 			rb.velocity = new Vector2(rb.velocity.x, jumpPower);
 			isJumping = true;
@@ -160,7 +154,7 @@ public class PlayerMovement : MonoBehaviour
 
 	private void bufferJump()
 	{
-		if(Input.GetKeyDown(KeyCode.Space) || Input.GetKey(KeyCode.W))
+		if(Input.GetKeyDown(KeyCode.Space))
 		{
 			bufferTimer = 0f;
 		}
@@ -175,7 +169,31 @@ public class PlayerMovement : MonoBehaviour
 		}
 	}
 
-	public void EnableJump()
+    public void KnockBackPlayer(float direction, float knockBackXForce, float knockBackYForce)
+    {
+        if (direction > 0)
+        {
+            print("Knockback ran");
+            player.AddForce(Vector2.left * knockBackXForce);
+
+            if (!isJumping)
+            {
+                player.AddForce(Vector2.up * knockBackYForce);
+            }
+        }
+
+        else
+        {
+            player.AddForce(Vector2.right * knockBackXForce);
+
+            if (!isJumping)
+            {
+                player.AddForce(Vector2.up * knockBackYForce);
+            }
+        }
+    }
+
+    public void EnableJump()
 	{
 		isGrounded = true;
 	}
@@ -184,7 +202,14 @@ public class PlayerMovement : MonoBehaviour
 	{
 		isGrounded = false;
 	}
+
+
+
 }
 
-	
+
+
+
+
+
 
